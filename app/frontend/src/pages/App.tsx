594e4f13365d71569977012a571025a4fb485a0a
Airam Toscano <airamlobato@gmail.com>
2023-01-03 21:08:52 -0400

    Feat: crianção do page httpCats.tsx

--- app/frontend/src/pages/App.tsx
+++ app/frontend/src/pages/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
+import HttpCats from './HttpCats';
 
 function App() {
   return (
     <Routes>
+      <Route path="/httpcat" element={<HttpCats />} />
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Navigate replace to="/login" />} />
